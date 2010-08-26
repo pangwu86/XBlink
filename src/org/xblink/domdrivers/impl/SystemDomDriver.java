1195c91ba5e206bc6bca40ccfb2f414e3b3f750f
pw <pangwu86@gmail.com>
2010-08-26 01:55:28 +0000

    DomDriver的注释添加
    SystemDomDriver的实现
    TODO 关于JDK的判断

--- src/org/xblink/domdrivers/impl/SystemDomDriver.java
+++ src/org/xblink/domdrivers/impl/SystemDomDriver.java
@@ -4,19 +4,23 @@ import org.xblink.adapter.XMLAdapter;
 import org.xblink.adapter.impl.SystemXMLAdapter;
 import org.xblink.domdrivers.DomDriver;
 
+/**
+ * JDK自带DOM驱动.
+ * 
+ * 
+ * @author E-Hunter(xjf1986518@gmail.com)
+ * @author pangwu86(pangwu86@gmail.com)
+ * 
+ */
 public class SystemDomDriver implements DomDriver {
 
-	@Override
 	public boolean canWork() {
-		// TODO Auto-generated method stub
+		// TODO 根据JDK版本去判断,1.5之前的貌似没有
 		return true;
 	}
 
-	@Override
 	public XMLAdapter getAdapter() {
-		// TODO Auto-generated method stub
 		return new SystemXMLAdapter();
 	}
 
-	
 }
