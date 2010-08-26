c277ce1063fca5b7e748d7a99ba98240c06d5856
pw <pangwu86@gmail.com>
2010-08-26 01:39:22 +0000

    传递过程中需要的信息 转移到 transferInfo 对象中

--- src/org/xblink/XType.java
+++ src/org/xblink/XType.java
@@ -1,14 +1,12 @@
 package org.xblink;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import org.w3c.dom.Node;
-import org.xblink.writer.XMLWriterUtil;
+import org.xblink.transfer.TransferInfo;
+import org.xblink.writer.XMLWriterHelper;
+import org.xblink.xml.XMLNode;
 
 /**
  * 
@@ -19,12 +17,6 @@ import org.xblink.writer.XMLWriterUtil;
  * 
  */
 public abstract class XType {
-
-	/** 类加载器切换器 */
-	protected ClassLoaderSwitcher classLoaderSwitcher;
-
-	/** 与传入的参数保存同步更新 */
-	protected ImplClasses xmlImplClasses;
 
 	/** 字段集合 */
 	protected List<Field> fieldTypes = new ArrayList<Field>();
@@ -40,22 +32,24 @@ public abstract class XType {
 	/**
 	 * 写项目.
 	 * 
-	 * @param writer
 	 * @param obj
+	 * @param writer
+	 * @param transferInfo
 	 * @throws Exception
 	 */
-	public abstract void writeItem(Object obj, XMLWriterUtil writer,
-			Map<Integer, ReferenceObject> referenceObjects) throws Exception;
+	public abstract void writeItem(Object obj, XMLWriterHelper writer, TransferInfo transferInfo)
+			throws Exception;
 
 	/**
 	 * 读项目.
 	 * 
 	 * @param obj
 	 * @param baseNode
+	 * @param transferInfo
 	 * @throws Exception
 	 */
-	public abstract void readItem(Object obj, Node baseNode,
-			Map<Integer, ReferenceObject> referenceObjects) throws Exception;
+	public abstract void readItem(Object obj, XMLNode baseNode, TransferInfo transferInfo)
+			throws Exception;
 
 	/**
 	 * 判断字段集合是否为空.
@@ -67,42 +61,6 @@ public abstract class XType {
 			return true;
 		}
 		return false;
-	}
-
-	/**
-	 * 获取类加载器切换器.
-	 * 
-	 * @return 类加载器切换器
-	 */
-	public ClassLoaderSwitcher getClassLoaderSwitcher() {
-		return classLoaderSwitcher;
-	}
-
-	/**
-	 * 设置类加载器切换器.
-	 * 
-	 * @param classLoaderSwitcher
-	 */
-	public void setClassLoaderSwitcher(ClassLoaderSwitcher classLoaderSwitcher) {
-		this.classLoaderSwitcher = classLoaderSwitcher;
-	}
-
-	/**
-	 * 获得实现类参数状态.
-	 * 
-	 * @param xmlImplClasses
-	 */
-	public void setImplClass(ImplClasses xmlImplClasses) {
-		this.xmlImplClasses = xmlImplClasses;
-	}
-
-	/**
-	 * 返回实现类集合.
-	 * 
-	 * @return 实现类集合
-	 */
-	public ImplClasses getImplClass() {
-		return xmlImplClasses;
 	}
 
 	/**
@@ -123,85 +81,4 @@ public abstract class XType {
 		return false;
 	}
 
-	/**
-	 * 内部类.<BR/>
-	 * Field的内部类型.
-	 * 
-	 * 
-	 * @author pangwu86(pangwu86@gmail.com)
-	 * 
-	 */
-	public class ClassType {
-		Type fieldInnerClassType;
-		Class<?> fieldInnerClass;
-		Class<?> fieldClass;
-
-		public Type getFieldInnerClassType() {
-			return fieldInnerClassType;
-		}
-
-		public void setFieldInnerClassType(Type fieldInnerClassType) {
-			this.fieldInnerClassType = fieldInnerClassType;
-		}
-
-		public Class<?> getFieldInnerClass() {
-			return fieldInnerClass;
-		}
-
-		public void setFieldInnerClass(Class<?> fieldInnerClass) {
-			this.fieldInnerClass = fieldInnerClass;
-		}
-
-		public Class<?> getFieldClass() {
-			return fieldClass;
-		}
-
-		public void setFieldClass(Class<?> fieldClass) {
-			this.fieldClass = fieldClass;
-		}
-	}
-
-	/**
-	 * 获得类类型.
-	 * 
-	 * @param field
-	 * @return 类类型
-	 */
-	public ClassType getClassType(Field field) {
-		Type type = null;
-		Class<?> fieldInnerClass = null;
-		Class<?> fieldClass = field.getType();
-		Type gtype = field.getGenericType();
-		if (gtype instanceof ParameterizedType) {
-			type = ((ParameterizedType) gtype).getActualTypeArguments()[0];
-		}
-		if (type != null) {
-			if (Constants.GENERICS.equals(type.toString())) {
-				// TODO 为了最外层的实现类 root层使用
-				fieldInnerClass = getImplClass().getNewInstanceClass();
-			} else {
-				if (type instanceof ParameterizedType) {
-					Type innerType = null;
-					innerType = ((ParameterizedType) type).getActualTypeArguments()[0];
-					if (innerType != null && Constants.GENERICS.equals(innerType.toString())) {
-						if (type.toString().startsWith(
-								Class.class.toString().replaceAll(Constants.CLASS_PREFIX,
-										Constants.EMPTY_STRING))) {
-							fieldInnerClass = Class.class;
-						} else {
-							// FIXME
-							fieldInnerClass = getImplClass().getNewInstanceClass();
-						}
-					}
-				} else {
-					fieldInnerClass = (Class<?>) type;
-				}
-			}
-		}
-		ClassType classType = new ClassType();
-		classType.setFieldInnerClassType(type);
-		classType.setFieldClass(fieldClass);
-		classType.setFieldInnerClass(fieldInnerClass);
-		return classType;
-	}
 }
