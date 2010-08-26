234ca924a79fba564df82409a5a2293ba6f34a60
pw <pangwu86@gmail.com>
2010-08-26 01:17:08 +0000

    ClassScannerTest 中 ClassScanner 修正为静态方法

--- test/org/xblink/util/ClassScannerTest.java
+++ test/org/xblink/util/ClassScannerTest.java
@@ -2,36 +2,35 @@ package org.xblink.util;
 
 import java.util.List;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 import org.xblink.XBlink;
 import org.xblink.types.XArray;
 import org.xblink.types.XAttribute;
 
-import junit.framework.TestCase;
-
 public class ClassScannerTest extends TestCase {
 
 	@Test
-	public void testPerformance() throws Exception{
-		ClassScanner classScanner = new ClassScanner();
+	public void testPerformance() throws Exception {
 		System.out.println("****第一次测试*****");
-		for(int i=0;i<100;i++){
+		for (int i = 0; i < 100; i++) {
 			WatchTimer watchTimer = new WatchTimer();
-			classScanner.scan(XArray.class);
+			ClassScannerUtil.scan(XArray.class);
 			System.out.println(watchTimer.getTimer());
 		}
 		System.out.println("****第二次测试****");
-		for(int i=0;i<100;i++){
+		for (int i = 0; i < 100; i++) {
 			WatchTimer watchTimer = new WatchTimer();
-			classScanner.scan(XBlink.class);
+			ClassScannerUtil.scan(XBlink.class);
 			System.out.println(watchTimer.getTimer());
 		}
 	}
+
 	@Test
-	public void testResults() throws Exception{
+	public void testResults() throws Exception {
 		List<Class<?>> list = null;
-		ClassScanner classScanner = new ClassScanner();
-		list=classScanner.scan(XAttribute.class);
+		list = ClassScannerUtil.scan(XAttribute.class);
 		for (Class<?> class1 : list) {
 			System.out.println(class1.getName());
 		}
