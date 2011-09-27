733bb4ddfa1f2204f5595064c72c96ce659dbf1e
pangwu86 <pangwu86@gmail.com>
2011-09-27 07:15:47 +0000

    测试用例添加

---
+++ test/otherpack/model/PhoneNumber.java
@@ -0,0 +1,22 @@
+package performance.model;
+
+public class PhoneNumber {
+	private int code;
+	private String number;
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+}
